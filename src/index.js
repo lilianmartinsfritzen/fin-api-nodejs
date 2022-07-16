b9d34f8e7dcb8602202e741adce8b39a07547e84
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-07-16 19:17:21 -0300

    feat: extract search by date

--- src/index.js
+++ src/index.js
@@ -96,4 +96,23 @@ app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).send()
 })
 
+app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+  const { date } = request.query
+
+  const dateFormat = new Date(date + ' 00:00')
+
+  /* se usar { } após o .filter É necessário informar o return
+  const statement = customer.statement.filter((statement) => {
+    return statement.created_at.toDateString() === new Date(dateFormat).toDateString()
+  }) */
+
+  // sem as { } após o .filter NÃO É necessário informar o return
+  const statement = customer.statement.filter((statement) => 
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString()
+  )
+
+  return response.json(statement)
+})
+
 app.listen(3333)
